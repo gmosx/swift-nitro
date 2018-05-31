4986c1e488624d2a1e9c88d82fd3b41b5d899dd5
George Moschovitis <gmosx@reizu.com>
2018-05-31 22:13:19 +0300

    Export a product, duh!

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "Nitro",
+    products: [
+        .library(name: "Nitro", targets: ["Nitro"])
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "1.0.0"),
         .package(url: "https://github.com/reizu/swift-logging.git", .branch("master")),
