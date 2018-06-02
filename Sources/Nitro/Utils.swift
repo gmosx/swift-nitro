c96bf553537b0ffdbd59492f44f441a10794bebf
George Moschovitis <gmosx@reizu.com>
2018-06-02 09:16:22 +0300

    Added simple urlencoded string decoder

---
+++ Sources/Nitro/Utils.swift
@@ -0,0 +1,23 @@
+
+public func decodeFormURLEncoded(queryString: String) -> [String: [String]] {
+    var values: [String: [String]] = [:]
+
+    for pair in queryString.split(separator: "&") {
+        let splitPair = pair.split(separator: "=", maxSplits: 1,
+                                   omittingEmptySubsequences: false)
+            .map { $0.replacingOccurrences(of: "+", with: " ") }
+            .map { $0.removingPercentEncoding ?? "??" } // TODO
+        let name  = splitPair[0]
+        let value = splitPair.count > 1 ? splitPair[1] : nil
+
+        if let value = value {
+            if values[name]?.append(value) == nil {
+                values[name] = [ value ]
+            }
+        } else if values[name] == nil {
+            values[name] = []
+        }
+    }
+
+    return values
+}
